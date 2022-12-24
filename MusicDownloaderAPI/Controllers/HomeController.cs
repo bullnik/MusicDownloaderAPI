a66666f3678fa34c0aa97daf95a536127e38fce5
Van Darkholme <jija@kaavo>
2022-12-24 07:29:24 +0500

    Add project files.

---
+++ MusicDownloaderAPI/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicDownloaderAPI.Models;
+using MusicDownloaderAPI.Rabbit;
+using System.Diagnostics;
+
+namespace MusicDownloaderAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IRabbitMqService _rabbitmq;
+
+        public HomeController(ILogger<HomeController> logger, IRabbitMqService rabbitMqService)
+        {
+            _logger = logger;
+            _rabbitmq = rabbitMqService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Download(string link)
+        {
+            return NotFound();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MusicDownloaderAPI/Rabbit/IRabbitMqService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MusicDownloaderAPI.Rabbit
+{
+    public interface IRabbitMqService
+    {
+        
+    }
+}
